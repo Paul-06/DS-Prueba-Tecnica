f7b48af221a3c0861785f34eba3e8e9bd86d53de
Paul-06 <80424124+Paul-06@users.noreply.github.com>
2024-06-19 00:40:10 -0500

    Creación del DbContext de la app

---
+++ Infrastructure/Data/AppDatabaseContext.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class AppDatabaseContext : DbContext
+    {
+        // Constructor
+        public AppDatabaseContext(DbContextOptions<AppDatabaseContext> options) : base(options)
+        {
+        }
+
+        // Entidades (tablas)
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<UsuarioSeguido> UsuarioSeguidos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Aquí agregare configuraciones adicionales para
+            // el tema de following
+        }
+    }
+}
