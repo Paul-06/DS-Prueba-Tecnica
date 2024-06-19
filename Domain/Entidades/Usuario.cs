923c3508d61b7e22f94e7126c9ef650f0f2df8b4
Paul-06 <80424124+Paul-06@users.noreply.github.com>
2024-06-19 17:15:28 -0500

    Agregado de la colección de seguido

--- Domain/Entidades/Usuario.cs
+++ Domain/Entidades/Usuario.cs
@@ -6,6 +6,7 @@
         public string? Username { get; set; }
         public DateTimeOffset FechaCreacion { get; set; }
         public virtual ICollection<Post>? PostRelationList { get; set; }
-        public virtual ICollection<UsuarioSeguido>? SeguidorRelationList { get; set; }
+        public virtual ICollection<UsuarioSeguido>? Seguidores { get; set; }
+        public virtual ICollection<UsuarioSeguido>? Seguidos { get; set; }
     }
 }
