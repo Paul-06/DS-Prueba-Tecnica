67d03dc77ffed861ca0f8f0d123881eaa30d9a78
Paul-06 <80424124+Paul-06@users.noreply.github.com>
2024-06-19 17:22:16 -0500

    Creación de LoadDatabase.cs para la carga inicial de datos

---
+++ Infrastructure/Data/LoadDatabase.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entidades;
+
+namespace Infrastructure.Data
+{
+    public class LoadDatabase
+    {
+        public static async Task InsertarData(AppDatabaseContext context)
+        {
+            if(!context.Usuarios.Any()) // Si hay usuarios, los agregamos
+            {
+                await context.Usuarios.AddRangeAsync(
+                    new Usuario { Username = "Alfonso" }, 
+                    new Usuario { Username = "Ivan" }, 
+                    new Usuario { Username = "Alicia" }
+                );
+
+                // Disparamos los cambios a la bd
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
