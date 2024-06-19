da62ce0583fb4bdca5ef8fd9fdff567b786e3391
Paul-06 <80424124+Paul-06@users.noreply.github.com>
2024-06-19 17:23:21 -0500

    Definición de endpoint CrearPost para pruebas

---
+++ WebApi/Controllers/PostController.cs
@@ -0,0 +1,28 @@
+﻿using Application.CasosUso;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Requests;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        // Implementaremos nuestro caso de uso "Crear Post"
+        private readonly CrearPost _crearPost;
+
+        // Constructor
+        public PostController(CrearPost crearPost)
+        {
+            _crearPost = crearPost;
+        }
+
+        // Metodos API
+        [HttpPost]
+        public IActionResult CrearPost([FromBody] CrearPostRequest request)
+        {
+            _crearPost.Ejecutar(request.IdUsuario, request.Contenido);
+            return Ok();
+        }
+    }
+}
